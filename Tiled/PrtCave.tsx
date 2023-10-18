2fb2e861824da273e3a95f14a8b792b00bbf225c
Daniel GV <danielgv.space@gmail.com>
2023-10-19 00:27:15 +0200

    tiled, muy muy interesante

---
+++ Tiled/PrtCave.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="PrtCave" tilewidth="16" tileheight="16" tilecount="80" columns="16">
+ <image source="../CVSource/PrtCave.png" trans="000000" width="256" height="80"/>
+</tileset>
